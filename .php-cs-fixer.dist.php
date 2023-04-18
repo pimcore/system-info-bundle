e4b063fa93f2b87b2ccf78cd302f5ed00e985d74
Martin Eiber <martin.eiber@pimcore.com>
2023-04-18 10:47:27 +0200

    Add php-cs-fixer action.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,13 +3,7 @@
 $finder = PhpCsFixer\Finder::create()
     ->in([
         __DIR__ . '/src',
-        __DIR__ . '/tests',
         __DIR__ . '/config'
-    ])
-
-    ->exclude([
-        __DIR__ . '/tests/_output',
-        __DIR__ . '/tests/Support/_generated',
     ])
 ;
 
